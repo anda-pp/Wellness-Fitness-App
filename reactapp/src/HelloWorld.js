14332bd4ff1b1491a42561270eeb2008ca6454bd
Your Name <you@example.com>
2024-11-13 18:01:30 +0200

    wip2

---
+++ reactapp/src/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import InputForm from './InputForm';
+
+function Helloworld(){
+return(
+    <div>
+        <h1>Welcome, world! </h1>
+        <p>Welcome to my first React app.</p>
+        <InputForm></InputForm>
+    </div>
+);
+
+}
+
+export default Helloworld;
---
+++ reactapp/src/InputForm.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+function InputForm(){
+return(
+    <div>
+         <input type="text" placeholder='Email'></input>
+         <input type="password" placeholder='Password'></input>
+    </div>
+);
+   
+}
+export default InputForm;
